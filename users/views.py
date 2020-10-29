ff680712878c359d6d06fb7383cbe27651866eda
Shivashish1010 <shivashish@naramdeo.com>
2020-10-29 09:44:53 -0400

    blog app

---
+++ users/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from .forms import UserRegisterForm
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, f'Account Created, you can login now!')
+            return redirect('login')
+    else:
+        form = UserRegisterForm()
+    return render(request , 'users/register.html' , {'form': form})
+
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html')
